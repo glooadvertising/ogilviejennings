8b9e6bce231fb93996c3d80e170827a0b45a47a8
Pablo Sazo <pablo@glooadvertising.com.au>
2024-02-19 13:56:56 +1100

    some images

--- partials/our-lawyers.php
+++ partials/our-lawyers.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="box-container">
                 <div class="featured-image">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/happy-young-businesswoman.jpg" alt="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/OJ_Insitu_Feb24_20240208_OJ_Feb24_Headshots_Insitu0049.jpg" alt="">
                 </div>
                 <div class="white-box">
                     <div class="text-group">
