a5508a599eabed9e63cf61cea3979b06e54d9d6c
Pablo Sazo <pablo@glooadvertising.com.au>
2024-02-20 15:37:09 +1100

    no menu order

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
                         'taxonomy' => 'role',
                         'post_type' => 'page',
                         'posts_per_page' => -1,
-                        'orderby' => ['taxonomy', 'menu_order'],
+                        'orderby' => ['taxonomy'],
                         'order' => 'ASC',
                         'tax_query' => array(
                             array(
