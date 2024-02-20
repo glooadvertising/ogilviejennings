62e92085a4311847ee916aba5879908d254da79c
Pablo Sazo <pablo@glooadvertising.com.au>
2024-02-20 15:39:23 +1100

    terms

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
                         'taxonomy' => 'role',
                         'post_type' => 'page',
                         'posts_per_page' => -1,
-                        'orderby' => ['taxonomy'],
+                        'orderby' => 'terms',
                         'order' => 'ASC',
                         'tax_query' => array(
                             array(
